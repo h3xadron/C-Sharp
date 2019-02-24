4aedcdb66238d942d8c7a09c3dad953078c03b13
Szeba <Szeba@DESKTOP-POD763C>
2019-02-24 14:38:01 +0100

    OOP Game

---
+++ Jatek/Jatek/Jatekter/JatekElem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jatek.Jatekter
+{
+    class JatekElem
+    {
+        private int x;
+        public int X { get { return x; } set { x = value; } }
+        private int y;
+        public int Y { get { return y; } set { y = value; } }
+
+        
+    }
+}
---
+++ Jatek/Jatek/Jatekter/JatekTer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jatek.Jatekter
+{
+    class JatekTer
+    {
+        const int MAX_ELEMSZAM = 1000;
+
+        private int elemN;
+        private JatekElem[] elemek; // ez most miért max_elemszám méretű így?!
+
+        private int meretX;
+        public int MeretX { get { return meretX; } }
+
+        private int meretY;
+        public int MeretY { get { return meretY; } }
+
+        public JatekTer(int meretX, int meretY)
+        {
+            this.meretY = meretX;
+            this.meretY = meretY;
+        }
+
+        public void Felvetel(JatekElem elemek) // ha nincs típus megadva akkor void?
+        {
+            
+            elemN++;
+
+        }
+
+        public void Torol(JatekElem elemek)
+        {
+            elemN--;
+        }
+
+        public JatekElem[] MegadottHelyenLevok(int x, int y, int tavolsag)
+        {
+
+            return JatekElem[]
+        }
+
+
+    }
+}

79431e422c6f4a39a5c77fbf5a408ee9466e1e14
szeba <szebasztian.szilagyi@gmail.com>
2023-03-25 20:57:55 +0100

    working

--- Memoria_Jatek_Workshop02/Memoria_Jatek_Workshop02/MainWindow.xaml.cs
+++ Memoria_Jatek_Workshop02/Memoria_Jatek_Workshop02/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,15 +22,16 @@ namespace Memoria_Jatek_Workshop02
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Label valasztas1;
+        private Label valasztas2;
+        int counter = 0;
         public MainWindow()
         {
 
             InitializeComponent();
         }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-         
             for (int i = 0; i < 2; i++)
             {
                 for ( int j = 1; j < 4; j++)
@@ -43,17 +45,51 @@ namespace Memoria_Jatek_Workshop02
                     Kartya.Tag = j;
                     WrapKartya.Children.Add(Kartya);
                     Kartya.MouseLeftButtonDown += Kartya_MouseLeftButtonDown;
+                    Kartya.MouseRightButtonDown += Kartya_MouseRightButtonDown;
                     if (j == 4) { j = 1; }
                }
-             
             }
         }
-
         private void Kartya_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Label kattintas = (Label)sender;
             var megjelnito = kattintas.Tag.ToString();
-            MessageBox.Show(megjelnito);
+            if (valasztas1 == null)
+            {
+                valasztas1 = (Label)e.Source;
+                valasztas1.Content = megjelnito;
+            }
+            else
+            {
+                valasztas2 = (Label)e.Source;
+                valasztas2.Content = megjelnito;
+            }
+        }
+        private void Kartya_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (counter < WrapKartya.Children.Count)
+            {
+                if ((valasztas1.Tag.ToString() != null) && (valasztas2.Tag.ToString() != null))
+                {
+                    if (valasztas1.Content == valasztas2.Content)
+                    {
+                        counter = counter + 2;
+                        valasztas1 = null;
+                        valasztas2 = null;
+                    }
+                    else
+                    {
+                        valasztas1.Content = "";
+                        valasztas2.Content = "";
+                        valasztas1 = null;
+                        valasztas2 = null;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("VÉGE");
+            }
         }
     }
 }
